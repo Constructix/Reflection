0c7a83e44aec454eb29df36f711432772b5ab5cd
Richard Jones <rjjones79@hotmail.com>
2022-08-07 12:03:45 +1000

    refactored files

---
+++ src/CallMethodDemo/Foo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+public class Foo
+{
+    public void Bar()
+    {
+        Console.WriteLine($"In {MethodInfo.GetCurrentMethod().Name}");
+    }
+
+    public void DisplayMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public void PassInPersonObject(Person person)
+    {
+        Console.WriteLine(person.ToString());
+    }
+}
+
---
+++ src/CallMethodDemo/Person.cs
@@ -0,0 +1,15 @@
+﻿public class Person
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+
+    public Person(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public override string ToString() => $"{this.FirstName} {this.LastName}";
+
+}
+
